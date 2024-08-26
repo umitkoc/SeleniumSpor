8f4994e6c1511c6eac361127ca66735f7577bb7e
umitkoc <umitkoc.com@gmail.com>
2024-08-26 20:20:04 +0300

    update program cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,32 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
-
-namespace WebApplication1;
-
-public static class Program
-{
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddAuthorization();
-
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapGet("/selenium-spor", ([FromBody]SeleniumSporRequest request) =>SporSelenium.Run(request));
-
-        app.Run();
-    }
-}
+using WebApplication1;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthorization();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+//
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapGet("/selenium-spor", ([FromBody]SeleniumSporRequest request) =>SporSelenium.Run(request));
+app.Run();
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04ff248795ff03224c5724dbacdd9a0446372478")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4bb22277e23dfaca74765069beeacabb6fa4b99")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
