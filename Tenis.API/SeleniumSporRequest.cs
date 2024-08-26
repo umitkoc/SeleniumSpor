b56669911538b824600b57709b04fe39e854cdea
umitkoc <umitkoc.com@gmail.com>
2024-08-26 21:23:59 +0300

    update name and service

---
+++ Tenis.API/SeleniumSporRequest.cs
@@ -0,0 +1,8 @@
+namespace WebApplication1;
+
+public class SeleniumSporRequest
+{
+    public string? tcnumber { get; set; }
+    public string? password { get; set; }
+    public bool all { get; set; }
+}
--- WebApplication1/SeleniumSporRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApplication1;
-
-public class SeleniumSporRequest
-{
-    public string? tcnumber { get; set; }
-    public string? password { get; set; }
-    public bool all { get; set; }
-}
